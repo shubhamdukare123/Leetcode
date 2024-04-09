d56ac5ef49ea64b480174158d78af75969414db0
AnujaGavhane07 <anujagavhane07@gmail.com>
2024-04-09 14:30:39 +0530

    day 80 queue implementatio using two stacks

---
+++ Day-80/gfg_Queue using two Stacks.java
@@ -0,0 +1,32 @@
+
+class StackQueue
+{
+    Stack<Integer> s1 = new Stack<Integer>();
+    Stack<Integer> s2 = new Stack<Integer>();
+
+    //Function to push an element in queue by using 2 stacks.
+    void Push(int x)
+    {
+	   // Your code here
+	   while(!s1.isEmpty()) {
+	       s2.push(s1.pop());
+	   }
+	   
+	   s2.push(x);
+	   
+	   while(!s2.isEmpty()) {
+	       s1.push(s2.pop());
+	   }
+    }
+	
+    
+    //Function to pop an element from queue by using 2 stacks.
+    int Pop()
+    {
+	   // Your code here
+	   if(s1.isEmpty()){
+	       return -1;
+	   }
+	   return s1.pop();
+    }
+}
