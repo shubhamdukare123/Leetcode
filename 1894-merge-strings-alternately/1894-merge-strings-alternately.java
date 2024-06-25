d29bf40719801593d700ee4bd89eb4e1e8c30a5d
Shubham Dukare <109910924+shubhamdukare123@users.noreply.github.com>
2024-06-25 14:30:05 +0530

    Time: 6 ms (28.7%), Space: 42.3 MB (26.82%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,25 +1,18 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int minLen = Math.min(word1.length(), word2.length());
-        String mergedStr = "";
-        for(int i=0; i < minLen; i++) {
-            //add characters into mergedStr
-            mergedStr += word1.charAt(0);
-            mergedStr += word2.charAt(0);
-            //removing added char from word1 and word2
-
-            word1 = new StringBuilder(word1).deleteCharAt(0).toString();
-            word2 = new StringBuilder(word2).deleteCharAt(0).toString();
-
+        int n1 = word1.length();
+        int n2 = word2.length();
+        String s = "";
+        int i=0;
+        while(i<n1 || i<n2) {
+            if(i<n1) {
+                s += word1.charAt(i);
+            }
+            if(i<n2) {
+                s += word2.charAt(i); 
+            }
+            i++;
         }
-
-
-        if(word1.length() > word2.length()) {
-            return mergedStr + word1;
-        }else if(word2.length() > word1.length()) {
-            return mergedStr + word2;
-        }else {
-            return mergedStr;
-        }
+        return s;
     }
 }
