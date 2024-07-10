2543ae343c8f9ceda3400a32ee330cf3117110f1
Shubham Dukare <109910924+shubhamdukare123@users.noreply.github.com>
2024-07-10 22:47:24 +0530

    Time: 1 ms (74.79%), Space: 41.9 MB (79.54%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -1,29 +1,30 @@
 class Solution {
-    public String gcdOfStrings(String str1, String str2) {
-        int str1Len = str1.length();
-        int str2Len = str2.length();
-        int minLen;
-        String minStr;
-        if(str1Len > str2Len) {
-            minLen = str2Len;
-            minStr = str2;
-        }else{
-            minLen = str1Len;
-            minStr = str1;
-        }
-        
-        while(minLen != 0) {
-            if(str1Len % minLen == 0 && str2Len % minLen == 0) {
-                String divStr = minStr.substring(0, minLen);
-                String str1Temp = divStr.repeat(str1Len/minLen);
-                String str2Temp = divStr.repeat(str2Len/minLen);
-                if(str1Temp.equals(str1) && str2Temp.equals(str2)) {
-                    return divStr;
-                }                
-            }
-            minLen--;
-        }
 
+    public String gcdOfStrings(String str1, String str2) {
+     int minLen;
+     int maxLen;
+
+     if(!(str1 + str2).equals(str2+str1)) {
         return "";
+     }
+     
+
+     if(str1.length() < str2.length()) {
+        minLen = str1.length();
+        maxLen = str2.length();
+        
+     }else{
+        minLen = str2.length();
+        maxLen = str1.length();
+     }
+
+     for(int i=minLen; i>0; i--) {
+        if(minLen % i == 0 && maxLen % i == 0) {
+            return str1.substring(0, i);
+        }
+     }
+
+     return "";
+
     }
 }
