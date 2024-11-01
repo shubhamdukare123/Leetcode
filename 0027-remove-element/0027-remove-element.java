7f54d4c5161fad63a7301abe792957ed3da73f8f
Shubham Dukare <109910924+shubhamdukare123@users.noreply.github.com>
2024-11-01 13:29:44 +0530

    Time: 0 ms (100%), Space: 42.1 MB (9.99%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,19 +1,13 @@
-import java.util.*;
 class Solution {
     public int removeElement(int[] nums, int val) {
-        List<Integer> lst = new ArrayList<>();
+        int count = 0;
 
         for(int i=0; i<nums.length; i++) {
             if(nums[i] != val) {
-                lst.add(nums[i]);
+                nums[count++] = nums[i];
             }
-        } 
-
-
-        for(int i=0; i<lst.size(); i++) {
-            nums[i] = lst.get(i);
         }
+        return count;
 
-        return lst.size();
     }
 }
